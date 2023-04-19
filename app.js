f677694a61963e1b4a81e040fd74e259206e5a79
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-19 12:02:39 +0700

    feat: create script for time

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const startBtn = document.querySelector('#start');
-
 const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+let time = 0;
 
 startBtn.addEventListener('click', (event) => {
   event.preventDefault();
   screens[0].classList.add('up');
 });
+
+timeList.addEventListener('click', (event) =>{
+  if (event.target.classList.contains('time-btn')) {
+    // console.log(event.target);
+    // console.log(event.target.getAttribute('data-time'));
+    time = parseInt(event.target.getAttribute('data-time'));
+  }
+})
