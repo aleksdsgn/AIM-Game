d043bac06bf2f6257a73885245d791a3cb94b7b7
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-19 11:54:55 +0700

    feat: create second screen

---
+++ app.js
@@ -0,0 +1,8 @@
+const startBtn = document.querySelector('#start');
+
+const screens = document.querySelectorAll('.screen');
+
+startBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  screens[0].classList.add('up');
+});
