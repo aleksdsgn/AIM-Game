a989df02b2299a51013658b504d7c3b66c0cda42
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-19 13:04:21 +0700

    feat: create timer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
-let time = 0;
+let time = 11;
+const timeEl = document.querySelector('#time');
 
 startBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -13,10 +14,37 @@ timeList.addEventListener('click', (event) =>{
     // console.log(event.target);
     // console.log(event.target.getAttribute('data-time'));
     time = parseInt(event.target.getAttribute('data-time'));
+    screens[1].classList.add('up');
     startGame();
   }
 })
 
+//DEBUG
+startGame();
+
 function startGame() {
-  screens[1].classList.add('up');
+  setInterval(decreaseTime, 1000);
+  setTime(time);
+}
+
+// обратный отсчет времени
+function decreaseTime() {
+  if (time === 0) {
+    finishGame();
+  } else {
+    let current = --time;
+    if (current < 10) {
+      current = `0${current}`;
+    }
+    setTime(current);
+  }
+}
+
+// вставка правильного времени в поле
+function setTime(value) {
+  timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+
 }
